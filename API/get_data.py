ecc0ca7319fa65bbafe867c37fb197055f5c5904
Abdelwahab <abdelwahabamr769@gmail.com>
2026-03-27 21:19:13 +0200

    scrap data and make expierment

---
+++ API/get_data.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+import os
+REPO = "microsoft/vscode" 
+AUTH_TOKEN =os.getenv("API_KEY") 
+headers = {"Authorization": f"token {AUTH_TOKEN}"}
+
+def get_issues(repo, num_pages=5):
+    issues = []
+    for page in range(1, num_pages + 1):
+        url = f"https://api.github.com/repos/{repo}/issues?state=closed&per_page=100&page={page}"
+        response = requests.get(url, headers=headers,timeout=30)
+        if response.status_code == 200:
+            data = response.json()
+            for item in data:
+                if "pull_request" not in item:
+                    labels = [l['name'] for l in item['labels']]
+                    if labels:
+                        issues.append({
+                            "title": item["title"],
+                            "body": item["body"],
+                            "label": labels[0]
+                        })
+        print(f"Finished page {page}")
+    return pd.DataFrame(issues)
+
+df = get_issues(REPO, num_pages=100)
+df.to_csv(r"Data\Scraped_data\github_issues.csv", index=False)
